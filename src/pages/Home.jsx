531a6e62dddfd705d986c00e7d2ea85d5489a0c6
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2024-12-05 05:41:47 -0500

    Menu V3.2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,29 @@ const Home = () => {
         setIsSidebarOpen={setIsSidebarOpen}
       />
       <div className="flex-1">
+        {/* Toolbar */}
+        <header className="bg-blue-600 text-white flex items-center h-16 px-4 shadow lg:hidden">
+          <button
+            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+            className="text-white focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            </svg>
+          </button>
+          <h1 className="ml-4 text-xl font-bold">Register Driver</h1>
+        </header>
         {/* Content */}
         <main className="p-4">
           <h1 className="text-4xl font-bold text-blue-900">Welcome to SECoV</h1>
