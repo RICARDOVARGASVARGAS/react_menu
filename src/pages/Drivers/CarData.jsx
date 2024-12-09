070d6a771e93cc4a2060cc6735e80ac2418380f3
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2024-12-09 12:42:35 -0500

    CarData v2

--- src/pages/Drivers/CarData.jsx
+++ src/pages/Drivers/CarData.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import Loading from "../../components/Loading"; // Componente de carga
-import { FaPlus, FaEdit } from "react-icons/fa";
+import { FaPlus, FaEdit, FaImage, FaFilePdf } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const CarData = ({ driverId }) => {
   const [vehicles, setVehicles] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [modalData, setModalData] = useState(null);
+  const [modalType, setModalType] = useState(""); // 'image' o 'pdf'
   const navigate = useNavigate(); // Para navegación entre páginas
 
   const fetchVehicles = async () => {
@@ -40,6 +42,27 @@ const CarData = ({ driverId }) => {
 
   const handleEditVehicle = (vehicleId) => {
     navigate(`/edit-car/${vehicleId}`);
+  };
+
+  const getFileType = (url) => {
+    if (url) {
+      const ext = url.split(".").pop().toLowerCase();
+      if (["jpg", "jpeg", "png", "gif"].includes(ext)) return "image";
+      if (ext === "pdf") return "pdf";
+      return "other"; // Para otros tipos de archivo, como Excel o Word
+    }
+    return null;
+  };
+
+  const handleOpenModal = (url) => {
+    const type = getFileType(url);
+    setModalData(url);
+    setModalType(type);
+  };
+
+  const closeModal = () => {
+    setModalData(null);
+    setModalType("");
   };
 
   if (loading) {
@@ -76,7 +99,9 @@ const CarData = ({ driverId }) => {
             <tbody>
               {vehicles.map((vehicle) => (
                 <tr key={vehicle.id}>
-                  <td className="border px-2 py-1 text-center">{vehicle.plate}</td>
+                  <td className="border px-2 py-1 text-center">
+                    {vehicle.plate}
+                  </td>
                   <td className="border px-2 py-1">
                     <div className="flex flex-col">
                       <span>
@@ -110,29 +135,69 @@ const CarData = ({ driverId }) => {
                       </span>
                     </div>
                   </td>
-                  <td className="border px-2 py-1 text-center">{vehicle.typeCar?.name}</td>
-                  <td className="border px-2 py-1 text-center">{vehicle.group?.name}</td>
+                  <td className="border px-2 py-1 text-center">
+                    {vehicle.typeCar?.name}
+                  </td>
+                  <td className="border px-2 py-1 text-center">
+                    {vehicle.group?.name}
+                  </td>
                   <td className="border px-2 py-1">
                     <div>
                       <span>{vehicle.number_soat}</span>
                       <div className="flex flex-col">
                         <span>
-                          <strong>F.Vig:</strong>  <br />{vehicle.date_soat_issue}
+                          <strong>F.Vig:</strong> <br />
+                          {vehicle.date_soat_issue}
                         </span>
                         <span>
-                          <strong>F.Ven:</strong><br />
+                          <strong>F.Ven:</strong>
+                          <br />
                           {vehicle.date_soat_expiration}
                         </span>
                       </div>
                     </div>
                   </td>
                   <td className="border px-2 py-1 text-center">
+                    {/* Botón Editar */}
                     <button
                       onClick={() => handleEditVehicle(vehicle.id)}
                       className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 flex items-center gap-1"
                     >
                       <FaEdit /> Editar
                     </button>
+                    {/* Condicional para el botón de Ver Foto */}
+                    {vehicle.file_car && vehicle.file_car !== "" ? (
+                      <button
+                        onClick={() => handleOpenModal(vehicle.file_car)}
+                        className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 flex items-center gap-1"
+                      >
+                        <FaImage /> Ver Foto
+                      </button>
+                    ) : null}{" "}
+                    {/* Si file_car está vacío, el botón no se renderiza */}
+                    {/* Condicional para el botón de Ver SOAT */}
+                    {vehicle.file_soat && vehicle.file_soat !== "" ? (
+                      <button
+                        onClick={() => handleOpenModal(vehicle.file_soat)}
+                        className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 flex items-center gap-1"
+                      >
+                        <FaFilePdf /> Ver SOAT
+                      </button>
+                    ) : null}{" "}
+                    {/* Si file_soat está vacío, el botón no se renderiza */}
+                    {/* Condicional para el botón de Ver Revisión Técnica */}
+                    {vehicle.file_technical_review &&
+                    vehicle.file_technical_review !== "" ? (
+                      <button
+                        onClick={() =>
+                          handleOpenModal(vehicle.file_technical_review)
+                        }
+                        className="bg-orange-500 text-white px-3 py-1 rounded hover:bg-orange-600 flex items-center gap-1"
+                      >
+                        <FaFilePdf /> Ver Revisión Técnica
+                      </button>
+                    ) : null}{" "}
+                    {/* Si file_technical_review está vacío, el botón no se renderiza */}
                   </td>
                 </tr>
               ))}
@@ -140,6 +205,45 @@ const CarData = ({ driverId }) => {
           </table>
         </div>
       </div>
+
+      {modalData && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg relative max-w-3xl w-full mx-4 sm:mx-auto sm:max-w-lg md:max-w-3xl">
+            {/* Botón de cierre mejorado */}
+            <button
+              onClick={closeModal}
+              className="absolute top-4 right-4 text-gray-600 hover:text-gray-800 text-2xl p-2 bg-gray-200 rounded-full shadow-md transition duration-200 ease-in-out"
+            >
+              <span className="font-semibold">×</span> {/* X en vez de texto */}
+            </button>
+
+            {/* Contenido del Modal */}
+            {modalType === "image" ? (
+              <div className="flex justify-center">
+                <img
+                  src={modalData}
+                  alt="Imagen del vehículo"
+                  className="w-full h-auto max-h-[80vh] object-contain rounded-lg"
+                />
+              </div>
+            ) : modalType === "pdf" ? (
+              <div className="flex justify-center">
+                <embed
+                  src={modalData}
+                  type="application/pdf"
+                  width="100%"
+                  height="600px"
+                  className="rounded-lg"
+                />
+              </div>
+            ) : (
+              <div className="flex justify-center">
+                <p>No se puede visualizar el archivo en este formato.</p>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
