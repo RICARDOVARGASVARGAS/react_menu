1c842ae758679d16ed14a29353c86552d8a6011f
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2025-01-10 10:24:51 -0500

    Permit

--- src/pages/Permits/ListPermits.jsx
+++ src/pages/Permits/ListPermits.jsx
@@ -1,258 +1,114 @@
 import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
-import Loading from "../../components/Loading"; // Componente de carga
+import Loading from "../../components/Loading";
 import {
   FaPlus,
-  FaEdit,
   FaFilePdf,
-  FaImage,
-  FaTrash,
   FaUpload,
   FaCheckCircle,
   FaExclamationCircle,
   FaTimes,
-  FaSave,
 } from "react-icons/fa";
-import {
-  API_BASE_URL,
-  API_STORAGE_URL,
-  TOKEN_API_STORAGE,
-} from "../../config/enviroments";
 import { AiOutlineClose } from "react-icons/ai";
+import { apiDelete, apiGet } from "../../services/apiService";
+import { useFileUploader, useFileDelete } from "../../hooks/useFileHook";
+import { extractUUID } from "../../utils/extractUUID";
+import RegisterPermit from "./RegisterPermit";
 
 const ListPermits = ({ onClose, carId }) => {
   const [permits, setPermits] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [showForm, setShowForm] = useState(false); // Estado para mostrar u ocultar el formulario
-  const [permitForm, setPermitForm] = useState({
-    issue_date: "",
-    expiration_date: "",
-    file_permit: "",
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+  const { uploadFile, isLoading: isFileUploading } = useFileUploader();
+  const { deleteFile, isLoading: isFileDeleting } = useFileDelete();
 
-  const [errors, setErrors] = useState({});
-
-  // Obtener la lista de permisos
-  const fetchPermits = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${API_BASE_URL}/getPermits/${carId}`);
-      const { data } = await response.json();
-      if (data) {
-        setPermits(data);
-      } else {
-        setPermits([]);
-        toast.info("El vehículo no tiene permisos registrados.");
-      }
-    } catch (error) {
-      toast.error("Error al cargar los licencias.");
-    } finally {
-      setLoading(false);
-    }
+  // Obtener la lista de seguros
+  const fetchItems = async () => {
+    setIsLoading(true);
+    const { data } = await apiGet(`getPermits/${carId}`);
+    setPermits(data);
+    setIsLoading(false);
   };
 
   useEffect(() => {
-    fetchPermits();
-  }, [carId]);
+    fetchItems();
+  }, []);
 
   const toggleForm = () => {
     setShowForm(!showForm);
   };
 
-  // Manejar cambios en los campos del formulario
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setPermitForm((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    setErrors({});
-
-    try {
-      const response = await fetch(`${API_BASE_URL}/registerPermit`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-        body: JSON.stringify({
-          ...permitForm,
-          car_id: carId,
-        }),
-      });
-
-      const { data, message, errors } = await response.json();
-
-      if (data) {
-        toast.success(message);
-        setShowForm(false);
-        setPermitForm({});
-        setErrors({});
-        fetchPermits();
-      } else {
-        console.log(errors);
-        toast.error(message);
-        setErrors(errors);
-      }
-    } catch (error) {
-      console.error("Error al registrar el seguro:", error);
-      toast.error("Ocurrió un error al realizar el seguro.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Subir SOAT
-  const uploadPermitFile = async (e, permitItem) => {
-    e.preventDefault();
-    setLoading(true);
-
+  // Subir Inspeccion
+  const uploadFileDocument = async (e, item) => {
     const file = e.target.files[0];
-
-    if (!file) {
-      toast.error("Por favor, seleccione un archivo.");
-      setLoading(false);
-      return;
-    }
-
-    // Validar tipo de archivo
-    if (file.type !== "application/pdf") {
-      toast.error("Solo se permiten archivos PDF.");
-      e.target.value = ""; // Limpiar el input
-      setLoading(false);
-      return;
-    }
-
-    // Validar tamaño del archivo (máximo 3 MB)
-    const maxSize = 3 * 1024 * 1024; // 3 MB en bytes
-    if (file.size > maxSize) {
-      toast.error("El archivo excede el tamaño máximo de 3 MB.");
-      e.target.value = ""; // Limpiar el input
-      setLoading(false);
-      return;
-    }
-
-    const formFile = new FormData();
-    formFile.append("file", file);
-    formFile.append("location", `cars/${carId}/permits/${permitItem.id}`);
-
     try {
-      const response = await fetch(`${API_STORAGE_URL}/files/upload`, {
-        method: "POST",
-        headers: {
-          Authorization: TOKEN_API_STORAGE,
+      await uploadFile({
+        file,
+        model: "Permit",
+        model_id: item.id,
+        model_storage: "file_permit",
+        storage: `Car/${carId}/Permit/${item.id}`,
+        onSuccess: (uploadedFile) => {
+          fetchItems();
         },
-        body: formFile,
+        onError: (errorMessage) => {
+          toast.error(errorMessage || "Error al subir el archivo.");
+        },
       });
-
-      const { message, errors, file } = await response.json();
-
-      if (file) {
-        permitItem.file_permit = file.url;
-        updatePermit(permitItem);
-      } else {
-        toast.error(message || "Ocurrió un error al subir el archivo.");
-      }
-    } catch (err) {
-      toast.error("Ocurrió un error al subir el archivo.");
-      console.error(err);
-    } finally {
-      setLoading(false);
+    } catch (error) {
+      console.error("Error inesperado al subir el archivo:", error);
     }
   };
 
-  const updatePermit = async (permitData) => {
+  // Eliminar la licencia
+  const deleteFileDocument = async (item) => {
+    const uuid = extractUUID(item.file_permit);
     try {
-      setLoading(true);
-      const response = await fetch(
-        `${API_BASE_URL}/updatePermit/${permitData.id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-          body: JSON.stringify({
-            ...permitData,
-          }),
-        }
-      );
+      await deleteFile({
+        model: "Permit",
+        model_id: item.id,
+        model_storage: "file_permit",
+        uuid: uuid,
+        onSuccess: (data) => {
+          toast.success("Elemento eliminado con éxito.");
+          fetchItems();
+        },
+        onError: (errorMessage) => {
+          toast.error(errorMessage || "Error al eliminar el Elemento.");
+        },
+      });
+    } catch (error) {
+      console.error("Error inesperado al eliminar el Elemento:", error);
+    }
+  };
 
-      const { data, message, errors } = await response.json();
+  const handleDelete = async (item) => {
+    setIsLoading(true);
+    if (item.file_permit) {
+      toast.error("No se puede eliminar  porque tiene un archivo adjunto.");
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      const response = await apiDelete(`deletePermit/${item.id}`);
+      const { data, message } = response;
       if (data) {
-        toast.success(message);
-        await fetchPermits();
+        toast.success(message || "Elemento eliminado.");
+        fetchItems();
       } else {
-        toast.error(message);
+        toast.error(message || "No se pudo eliminar el Elemento.");
       }
     } catch (error) {
+      toast.error("No se pudo eliminar el Elemento.");
       console.log(error);
-      toast.error("Error al actualizar el Permiso.");
     } finally {
-      setLoading(false);
+      setIsLoading(false);
     }
-  };
-
-  const deletePermit = async (permitId) => {
-    try {
-      setLoading(true);
-      const response = await fetch(`${API_BASE_URL}/deletePermit/${permitId}`, {
-        method: "DELETE",
-      });
-
-      const { data, message, errors } = await response.json();
-      if (data) {
-        if (data.file_permit) {
-          deletePermitFile(data);
-        } else {
-          toast.success(message);
-          await fetchPermits();
-        }
-      } else {
-        toast.error(message);
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("Error al eliminar el seguro.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Eliminar el seguro
-  const deletePermitFile = async (permitItem) => {
-    setLoading(true);
-    const encodedUrl = btoa(permitItem.file_permit);
-    const response = await fetch(`${API_STORAGE_URL}/files/${encodedUrl}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-        Authorization: TOKEN_API_STORAGE,
-      },
-      body: JSON.stringify({}),
-    });
-
-    const { file, message, errors } = await response.json();
-    if (file) {
-      permitItem.file_permit = null;
-      toast.success("Seguro eliminado.");
-      await fetchPermits();
-      // updatePermit(permitItem);
-    } else {
-      toast.error(message);
-    }
-
-    setLoading(false);
   };
   return (
     <>
-      {loading && <Loading />}
+      {isLoading && <Loading />}
       <div className="bg-white shadow rounded-lg overflow-y-auto max-h-[80vh]">
         <div className="p-4 flex justify-between items-center">
           <button
@@ -262,7 +118,7 @@ const ListPermits = ({ onClose, carId }) => {
             <AiOutlineClose size={20} />
           </button>
           <h2 className="text-xl font-bold text-gray-800">
-            Permisos de Circulación{" "}
+            Permisos de Circulación
           </h2>
           <button
             onClick={toggleForm}
@@ -283,71 +139,12 @@ const ListPermits = ({ onClose, carId }) => {
         </div>
         {/* Formulario de registro */}
         {showForm && (
-          <div className="p-4 border-t bg-gray-50">
-            <h3 className="text-lg font-semibold text-gray-700 mb-3">
-              Registrar Nuevo Seguro
-            </h3>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
-                <div>
-                  <label className="block text-sm font-semibold">
-                    Fecha de Emision
-                  </label>
-                  <input
-                    type="date"
-                    id="issue_date"
-                    name="issue_date"
-                    value={permitForm.issue_date || ""}
-                    autoComplete="off"
-                    onChange={handleChange}
-                    className={`mt-1 p-2 w-full border rounded ${
-                      errors.issue_date ? "border-red-500" : ""
-                    }`}
-                  />
-                  {errors.issue_date && (
-                    <p className="text-red-500 text-sm">{errors.issue_date}</p>
-                  )}
-                </div>
-
-                <div>
-                  <label className="block text-sm font-semibold">
-                    Fecha de Vencimiento
-                  </label>
-                  <input
-                    type="date"
-                    id="expiration_date"
-                    name="expiration_date"
-                    value={permitForm.expiration_date || ""}
-                    autoComplete="off"
-                    onChange={handleChange}
-                    className={`mt-1 p-2 w-full border rounded ${
-                      errors.expiration_date ? "border-red-500" : ""
-                    }`}
-                  />
-                  {errors.expiration_date && (
-                    <p className="text-red-500 text-sm">
-                      {errors.expiration_date}
-                    </p>
-                  )}
-                </div>
-              </div>
-
-              <div className="flex justify-end mt-6 gap-4">
-                <button
-                  type="button"
-                  className="bg-gray-600 text-white py-2 px-6 rounded"
-                >
-                  Cerrar
-                </button>
-                <button
-                  type="submit"
-                  className="bg-blue-600 text-white py-2 px-6 rounded flex items-center gap-2"
-                >
-                  <FaSave /> Guardar
-                </button>
-              </div>
-            </form>
-          </div>
+          <RegisterPermit
+            toggleForm={toggleForm}
+            cardId={carId}
+            setIsLoading={setIsLoading}
+            fetchItems={fetchItems}
+          />
         )}
         {/* Tabla de permisos */}
         <div className="overflow-x-auto">
@@ -402,14 +199,14 @@ const ListPermits = ({ onClose, carId }) => {
                           id={`permitInput-${permit.id}`} // ID único basado en el ID de la licencia
                           className="hidden"
                           accept="application/pdf"
-                          onChange={(e) => uploadPermitFile(e, { ...permit })}
-                          disabled={loading}
+                          onChange={(e) => uploadFileDocument(e, { ...permit })}
+                          disabled={isLoading}
                         />
                         <label
                           htmlFor={`permitInput-${permit.id}`} // Ajuste en el for del label
                           className="inline-flex items-center justify-center cursor-pointer bg-gray-200 text-gray-700 text-sm px-4 py-2 rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-opacity-50 transition duration-300 ease-in-out"
                         >
-                          {loading ? (
+                          {isLoading ? (
                             "Subiendo..."
                           ) : (
                             <>
@@ -421,14 +218,21 @@ const ListPermits = ({ onClose, carId }) => {
                       </>
                     )}
                   </td>
-                  <td className="border px-2 py-1 text-center">
-                    {" "}
+                  <td className="border px-2 py-1 text-center flex flex-col gap-2">
                     <button
-                      onClick={() => deletePermit(permit.id)}
+                      onClick={() => handleDelete(permit)}
                       className="bg-red-600 hover:bg-red-700 text-white py-1 px-4 rounded"
                     >
-                      Eliminar
+                      Eliminar Permiso
                     </button>
+                    {permit.file_permit && (
+                      <button
+                        onClick={() => deleteFileDocument(permit)}
+                        className="bg-red-600 hover:bg-red-700 text-white py-1 px-4 rounded"
+                      >
+                        Eliminar Documento
+                      </button>
+                    )}
                   </td>
                 </tr>
               ))}
---
+++ src/pages/Permits/RegisterPermit.jsx
@@ -0,0 +1,126 @@
+import { useForm } from "react-hook-form";
+import { FaSave } from "react-icons/fa";
+import { handleBackendErrors } from "../../utils/handleBackendErrors ";
+import { apiPost } from "../../services/apiService";
+import { toast } from "react-toastify";
+const RegisterPermit = ({ toggleForm, setIsLoading, cardId, fetchItems }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset,
+    watch,
+  } = useForm({
+    defaultValues: {
+      issue_date: "",
+      expiration_date: "",
+      file_permit: "",
+      car_id: cardId,
+    },
+  });
+
+  const onSubmit = handleSubmit(async (formData) => {
+    setIsLoading(true);
+    try {
+      const response = await apiPost("registerPermit", formData);
+      const { data, message } = response;
+
+      if (data) {
+        toggleForm();
+        reset();
+        fetchItems();
+        toast.success(message);
+      } else {
+        toast.error(message);
+      }
+    } catch (error) {
+      handleBackendErrors(error, setError);
+    } finally {
+      setIsLoading(false);
+    }
+  });
+
+  return (
+    <>
+      <div className="p-4 border-t bg-gray-50">
+        <h3 className="text-lg font-semibold text-gray-700 mb-3">
+          Registrar Circulación
+        </h3>
+        <form onSubmit={onSubmit} className="space-y-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-4">
+            <div>
+              <label className="block text-sm font-semibold">
+                Fecha de Emisión
+              </label>
+              <input
+                type="date"
+                name="issue_date"
+                autoComplete="off"
+                className={`mt-1 p-2 w-full border rounded ${
+                  errors.issue_date ? "border-red-500" : ""
+                }`}
+                {...register("issue_date", {
+                  required: {
+                    value: true,
+                    message: "El Fecha de Emisión es requerido",
+                  },
+                })}
+              />
+
+              {errors.issue_date && (
+                <p className="text-red-500 text-sm">
+                  {errors.issue_date.message}
+                </p>
+              )}
+            </div>
+
+            <div>
+              <label className="block text-sm font-semibold">
+                Fecha de Revalidación
+              </label>
+              <input
+                type="date"
+                name="expiration_date"
+                autoComplete="off"
+                className={`mt-1 p-2 w-full border rounded ${
+                  errors.expiration_date ? "border-red-500" : ""
+                }`}
+                {...register("expiration_date", {
+                  required: {
+                    value: true,
+                    message: "El Fecha de Revalidación es requerido",
+                  },
+                })}
+              />
+
+              {errors.expiration_date && (
+                <p className="text-red-500 text-sm">
+                  {errors.expiration_date.message}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="flex justify-end mt-6 gap-4">
+            <button
+              onClick={toggleForm}
+              type="button"
+              className="bg-gray-600 text-white py-2 px-6 rounded"
+            >
+              Cerrar
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-600 text-white py-2 px-6 rounded flex items-center gap-2"
+            >
+              <FaSave /> Guardar
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default RegisterPermit;
