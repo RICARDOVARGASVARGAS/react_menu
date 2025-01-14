166934760596c56d850e67ba6f732df32e15b58d
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2025-01-14 09:34:08 -0500

    token

--- src/config/enviroments.js
+++ src/config/enviroments.js
@@ -8,7 +8,7 @@ export const TOKEN_API_STORAGE =
   import.meta.env.VITE_TOKEN_API_STORAGE ||
   (import.meta.env.MODE === "development"
     ? "123456789" // URL en desarrollo
-    : "91914f0f-9c64-4f4b-b383-64616b41bb0d"); // URL en producción
+    : "9268b146-b9ba-49a4-a8ed-4443697f2c95"); // URL en producción
 
 export const API_DATA_PEOPLE_URL =
   "https://data-people.sys-code.com/api/getPerson"; // URL de la API de personas
