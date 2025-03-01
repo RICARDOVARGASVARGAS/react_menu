00cc224d19fa4a2c35d84ded493fad29aa72ecd2
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-03-01 15:22:47 -0500

    User

--- src/validations/userSchema.js
+++ src/validations/userSchema.js
@@ -12,12 +12,12 @@ export const userSchema = z.object({
     .max(50, "El Nombre no debe exceder los 50 caracteres"),
   first_name: z
     .string()
-    .min(3, "El Apellido debe tener al menos 3 caracteres")
-    .max(50, "El Apellido no debe exceder los 50 caracteres"),
+    .min(3, "El Apellido Paterno debe tener al menos 3 caracteres")
+    .max(50, "El Apellido Paterno no debe exceder los 50 caracteres"),
   last_name: z
     .string()
-    .min(3, "El Apellido debe tener al menos 3 caracteres")
-    .max(50, "El Apellido no debe exceder los 50 caracteres"),
+    .min(3, "El Apellido Materno debe tener al menos 3 caracteres")
+    .max(50, "El Apellido Materno no debe exceder los 50 caracteres"),
   email: z
     .string()
     .email("El correo no es valido")
@@ -25,6 +25,6 @@ export const userSchema = z.object({
     .max(50, "El correo no debe exceder los 50 caracteres"),
   phone_number: z
     .string()
-    .min(8, "El telefono debe tener al menos 8 caracteres")
-    .max(8, "El telefono no debe exceder los 8 caracteres"),
+    .min(9, "El telefono debe tener al menos 8 caracteres")
+    .max(20, "El telefono no debe exceder los 20 caracteres"),
 });
