8c2ffd1a258cd612656ba36a968f6324ceee1418
Ricardo Vargas V <vargasvargas.ric@gmail.com>
2025-03-14 12:36:44 -0500

    Zod

--- src/validations/driverSchema.js
+++ src/validations/driverSchema.js
@@ -34,6 +34,7 @@ export const driverSchemaRegister = z.object({
     .email("El correo no es válido") // Validación de correo primero
     .min(3, "El correo debe tener al menos 3 caracteres")
     .max(50, "El correo no debe exceder los 50 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
   phone_number: z
@@ -41,15 +42,17 @@ export const driverSchemaRegister = z.object({
     .regex(/^\d+$/, "El teléfono debe contener solo números") // Validación de números primero
     .min(9, "El teléfono debe tener al menos 9 caracteres")
     .max(20, "El teléfono no debe exceder los 20 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
   address: z
     .string()
     .min(3, "La Dirección debe tener al menos 3 caracteres")
     .max(50, "La Dirección no debe exceder los 50 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
-  gender: z.string().refine((value) => ["m", "f"].includes(value), {
+  gender: z.string().refine((value) => ["M", "F"].includes(value), {
     message: "El género debe ser 'm' o 'f'",
   }),
 });
@@ -88,6 +91,7 @@ export const driverSchemaEdit = z.object({
     .email("El correo no es válido") // Validación de correo primero
     .min(3, "El correo debe tener al menos 3 caracteres")
     .max(50, "El correo no debe exceder los 50 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
   phone_number: z
@@ -95,15 +99,17 @@ export const driverSchemaEdit = z.object({
     .regex(/^\d+$/, "El teléfono debe contener solo números") // Validación de números primero
     .min(9, "El teléfono debe tener al menos 9 caracteres")
     .max(20, "El teléfono no debe exceder los 20 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
   address: z
     .string()
     .min(3, "La Dirección debe tener al menos 3 caracteres")
     .max(50, "La Dirección no debe exceder los 50 caracteres")
+    .or(z.literal("")) // Permite cadenas vacías
     .nullable() // Permitir null después de las validaciones
     .optional(), // Opcional (puede estar ausente)
-  gender: z.string().refine((value) => ["m", "f"].includes(value), {
+  gender: z.string().refine((value) => ["M", "F"].includes(value), {
     message: "El género debe ser 'm' o 'f'",
   }),
 });
